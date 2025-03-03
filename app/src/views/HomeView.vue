c82eb525be006c862c1fb6df83202ad9226244d9
joeyshi07 <joeys19@nycstudents.net>
2025-03-03 14:00:33 -0500

    Update HomeView.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
@@ -6,4 +6,23 @@ import TheWelcome from '../components/TheWelcome.vue'
   <main>
     <TheWelcome />
   </main>
+</template> -->
+<template>
+  <div class="card" v-for="(score,index) in satScores" :key = </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const satScores = ref('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
+async function getData() {
+  let res = await fetch('')
+  let data = await res.json()
+  satScores.value = data.results
+}
+
+onMounted(() => {
+  getData()
+})
+</script>
+
+<style lang="scss" scoped></style>
